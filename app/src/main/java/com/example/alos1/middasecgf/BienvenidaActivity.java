cbc8562cf3ea984698a5ef14b10e7d177864bd10
alos1895 <alos1895@gmail.com>
2018-11-25 23:16:25 -0600

    Aplicacion de prototipo
    Solo muestra movimiento en pantallas sin ninguna funcionalidad aparente

--- app/src/main/java/com/example/alos1/middasecgf/BienvenidaActivity.java
+++ app/src/main/java/com/example/alos1/middasecgf/BienvenidaActivity.java
@@ -1,7 +1,10 @@
 package com.example.alos1.middasecgf;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BienvenidaActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class BienvenidaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bienvenida);
+
+        Button comenzar = (Button)findViewById(R.id.btnComenzar);
+
+        comenzar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ListSong = new Intent(getApplicationContext(), ConexionActivity.class);
+                startActivity(ListSong);
+            }
+        });
     }
 }
